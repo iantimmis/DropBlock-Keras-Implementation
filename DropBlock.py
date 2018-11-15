65d2a3ec9a371fffd76caa35642dba0710d3a9be
iantimmis <itimmis@ltu.edu>
2018-11-15 14:50:49 -0500

    removed unecessary comment

--- DropBlock.py
+++ DropBlock.py
@@ -6,7 +6,7 @@ import copy
 class DropBlock(Layer):
     """
     Regularization Technique for Convolutional Layers.
-    
+
     Pseudocode:
     1: Input:output activations of a layer (A), block_size, γ, mode
     2: if mode == Inference then
@@ -17,11 +17,10 @@ class DropBlock(Layer):
         height being block_size and set all the values of M in the square to be zero (see Figure 2).
     7: Apply the mask: A = A × M
     8: Normalize the features: A = A × count(M)/count_ones(M)
-    
+
     # Arguments
         block_size: A Python integer. The size of the block to be dropped.
         gamma: float between 0 and 1. controls how many activation units to drop.
-        seed: A Python integer to use as random seed.
     # References
         - [DropBlock: A regularization method for convolutional networks](
            https://arxiv.org/pdf/1810.12890v1.pdf)
@@ -32,52 +31,52 @@ class DropBlock(Layer):
         self.keep_prob = keep_prob
 
     def call(self, x, training=None):
-        
+
         # During inference, we do not Drop Blocks. (Similar to DropOut)
         if training == None:
             return x
-        
+
         # Calculate Gamma
         feat_size = int(x.shape[-1])
         gamma = ((1-self.keep_prob)/(self.block_size**2)) * ((feat_size**2) / ((feat_size-self.block_size+1)**2))
-        
+
         padding = self.block_size//2
-        
+
         # Randomly sample mask
         sample = bernoulli.rvs(size=(feat_size-(padding*2), feat_size-(padding*2)),p=gamma)
-        
+
         # The above code creates a matrix of zeros and samples ones from the distribution
         # We would like to flip all of these values
         sample = 1-sample
-    
+
         # Pad the mask with ones
         sample = np.pad(sample, pad_width=padding, mode='constant', constant_values=1)
-    
+
         # For each 0, create spatial square mask of shape (block_size x block_size)
         mask = copy.copy(sample)
         for i in range(feat_size):
             for j in range(feat_size):
                 if sample[i, j]==0:
                     mask[i-padding : i+padding+1, j-padding : j+padding+1] = 0
-        
+
         mask = mask.reshape((1, feat_size, feat_size))
-        
+
         # Apply the mask
         x = x * np.repeat(mask, x.shape[1], 0)
-        
+
         # Normalize the features
         count = np.prod(mask.shape)
         count_ones = np.count_nonzero(mask == 1)
         x = x * count / count_ones
-        
+
         return x
-        
+
     def get_config(self):
         config = {'block_size': self.block_size,
                   'gamma': self.gamma,
                   'seed': self.seed}
         base_config = super(DropBlock, self).get_config()
         return dict(list(base_config.items()) + list(config.items()))
-        
+
     def compute_output_shape(self, input_shape):
         return input_shape
